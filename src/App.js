cde28a798c88879f0add102928d424f02360e967
santony2 <santony2@hawk.iit.edu>
2024-12-01 20:34:57 -0800

    Added "Add" functionality and item display
    Added input type for item display for check box, text, edit, and delete buttons. Also added functionality for adding items to the list.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,41 @@
 //import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [todoitems, setTodoitems] = useState([]);
+  const [inputValue, setInputValue] = useState('');
+  
+  function handleChange(e){
+    setInputValue(e.target.value)
+  }
+  
+  function handleSubmit(e){
+    e.preventDefault()
+    setTodoitems([...todoitems, inputValue])
+    setInputValue('')
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>To Do list</h1>
         <p>
-          Items To Do
+          Here's a list of items for you to do
         </p>
       </header>
       <body className="App-body">
-      <form>
-        <input type="text"></input>
-        <button>Add To Do</button>
+      <form><br />
+        {todoitems.map((todo) => (
+        <><input type="checkbox" id={todo} name={todo} value={todo}></input><label>{todo}&nbsp;</label>
+         <button>Edit</button>&nbsp;
+         <button>Delete</button><br/>
+        </>
+        ))}
+        <br />
+        <input type="text" value={inputValue} onChange={handleChange}></input>
+        <button onClick={handleSubmit}>Add To Do</button>
+        <br />
       </form>
       </body>
     </div>
