8c41e9a0251538ce3bc1048ae1e8337c683f2f47
santony2 <santony2@hawk.iit.edu>
2024-12-01 19:53:33 -0800

    Step 1 - Create app framework
    Created a framework by editing the app.js and app.css files for styling the body tag.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <h1>To Do list</h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Items To Do
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <body className="App-body">
+      <form>
+        <input type="text"></input>
+        <button>Add To Do</button>
+      </form>
+      </body>
     </div>
   );
 }
